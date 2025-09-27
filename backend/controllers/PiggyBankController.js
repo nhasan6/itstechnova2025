136b7a4ad55bbec82cef1e3b1fd9b68551c286f4
Neeya <169307919+nhasan6@users.noreply.github.com>
2025-09-27 14:15:19 -0400

    edit controllers

--- backend/controllers/PiggyBankController.js
+++ backend/controllers/PiggyBankController.js
@@ -2,10 +2,10 @@ const PiggyBank = require('../models/PiggyBank')
 
 const getPiggyBanks = async (req,res) => {
     try {
-        const piggybanks = PiggyBank.find();
+        const piggybanks = await PiggyBank.find();
         res.status(200).json(piggybanks);
     } catch (err) {
-        res.status(500).json({message: 'Error fetching transactions'});
+        res.status(500).json({message: 'Error fetching piggy banks'});
     }
 }
 
@@ -13,31 +13,45 @@ const getPiggyBankById = async (req, res) => {
     try {
         const piggybank = await PiggyBank.findById(req.params.id);
         if (!piggybank) {
-            res.status(404).json({error: 'not found'});
-        } else {
-            res.status(200).json(piggybank);
+            return res.status(404).json({error: 'not found'});
         } 
+        res.status(200).json(piggybank);
     } catch (err) {
-        res.status(500).json({error: err.message}); x
+        res.status(500).json({error: err.message}); 
     }
 }
 
 const createPiggyBank = async (req, res) => {
     try {
         const piggyBank = await PiggyBank.create(req.body);
-        res.status(201);
+        res.status(201).json(piggyBank);
     } catch (err) {
         res.status(400).json({error: err.message});
     }
 }
 
-const addFunds = async (req, res) => {
+const openBank = async (req,res) => {
     try {
+        const { piggyBankId } = req.body;
+        const piggyBank = await PiggyBank.findById(piggyBankId);
+
+        if (!piggyBank) {
+            return res.status(404).json({error: 'not found'});
+        }
+
+        if ((piggyBank.balance >= piggyBank.goal) && !piggyBank.opened) {
+            piggyBank.opened = true;
+            piggyBank.balance -= piggyBank.goal;
+        }
+        await piggyBank.save();
+        res.status(200).json(piggyBank);
     } catch (err) {
-        res.status()
+        res.status(500).json({error: err.message})
     }
 }
-
-const allocateBalance = async (req,res) => {}
-const  = async (req,res) => {}
-const openBank = async (req,res) => {}
+module.exports = {
+    getPiggyBanks,
+    getPiggyBankById,
+    createPiggyBank,
+    openBank
+};
--- backend/controllers/TransactionController.js
+++ backend/controllers/TransactionController.js
@@ -1,8 +1,11 @@
 const Transaction = require('../models/Transaction') 
+const PiggyBank = require('../models/PiggyBank') 
 
+
+// get all transactions
 const getTransactions = async (req,res) => {
     try {
-        const transactions = Transaction.find();
+        const transactions = await Transaction.find();
         res.status(200).json(transactions);
     } catch (err) {
         res.status(500).json({message: 'Error fetching transactions'});
@@ -13,31 +16,46 @@ const getTransactionById = async (req, res) => {
     try {
         const transaction = await Transaction.findById(req.params.id);
         if (!transaction) {
-            res.status(404).json({error: 'not found'});
-        } else {
-            res.status(200).json(transaction);
+            return res.status(404).json({error: 'not found'});
         } 
+        res.status(200).json(transaction);
     } catch (err) {
-        res.status(500).json({error: err.message}); x
+        res.status(500).json({error: err.message}); 
     }
 }
 
 const createTransaction = async (req, res) => {
     try {
         const piggyBank = await Transaction.create(req.body);
-        res.status(201);
+        res.status(201).json(piggyBank);
     } catch (err) {
         res.status(400).json({error: err.message});
     }
 }
 
-const addFunds = async (req, res) => {
+const allocateBalance = async (req,res) => {
     try {
+        const { piggyBankId, amount } = req.body;
+
+        const piggyBank = await Transaction.findById(piggyBankId);
+
+        if (!piggyBank) {
+            return res.status(404).json({error: 'not found'});
+        }
+        piggyBank.balance = piggyBank.balance + amount;
+
+        await piggyBank.save()
+
+        res.status(200).json(piggyBank);
     } catch (err) {
-        res.status()
+        res.status(500).json({error: err.message})
     }
 }
 
-const allocateBalance = async (req,res) => {}
-const  = async (req,res) => {}
-const openBank = async (req,res) => {}
+module.exports = {
+    getTransactions,
+    getTransactionById,
+    createTransaction,
+    allocateBalance,
+    openBank
+};
